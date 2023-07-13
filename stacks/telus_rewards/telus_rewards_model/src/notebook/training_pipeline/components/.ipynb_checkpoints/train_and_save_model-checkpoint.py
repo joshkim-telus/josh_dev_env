9c9aeef116141a13a728446c89f17c2a62f0454f
Josh Kim <josh.kim@telus.com>
2023-07-13 14:13:37 +0000

    sync 20230713

--- stacks/telus_rewards/telus_rewards_model/src/notebook/training_pipeline/components/.ipynb_checkpoints/train_and_save_model-checkpoint.py
+++ stacks/telus_rewards/telus_rewards_model/src/notebook/training_pipeline/components/.ipynb_checkpoints/train_and_save_model-checkpoint.py
@@ -55,7 +55,7 @@ def train_and_save_model(
     df_target_train = client.query(sql_train).to_dataframe()
     # df_target_train = df_target_train.loc[
     #     df_target_train['YEAR_MONTH'] == '-'.join(score_date_dash.split('-')[:2])]  # score_date_dash = '2022-08-31'
-    df_target_train = df_target_train.loc[df_target_train['YEAR_MONTH'] == '2022-H2']  # score_date_dash = '2022-08-31'
+    df_target_train = df_target_train.loc[df_target_train['YEAR_MONTH'] == '2022-Q3']  # score_date_dash = '2022-08-31'
     df_target_train['ban'] = df_target_train['ban'].astype('int64')
     df_target_train = df_target_train.groupby('ban').tail(1)
     df_train = df_train.merge(df_target_train[['ban', 'target_ind']], on='ban', how='left')
@@ -110,34 +110,20 @@ def train_and_save_model(
     import xgboost as xgb
     from sklearn.metrics import roc_auc_score
 
-    # xgb_model = xgb.XGBClassifier(
-    #     learning_rate=0.01,
-    #     n_estimators=100,
-    #     max_depth=8,
-    #     min_child_weight=1,
-    #     gamma=0,
-    #     subsample=0.8,
-    #     colsample_bytree=0.8,
-    #     objective='binary:logistic',
-    #     nthread=4,
-    #     scale_pos_weight=1
-    #     # seed=27
-    # )
-
     xgb_model = xgb.XGBClassifier(
-        learning_rate=0.02,
-        n_estimators=1000,
-        max_depth=10,
+        learning_rate=0.05,
+        n_estimators=100,
+        max_depth=8,
         min_child_weight=1,
         gamma=0,
         subsample=0.8,
         colsample_bytree=0.8,
         objective='binary:logistic',
         nthread=4,
-        scale_pos_weight=1,
-        seed=27
+        scale_pos_weight=1
+        # seed=27
     )
-    
+
     xgb_model.fit(X_train, y_train)
     print('xgb training done')
 
